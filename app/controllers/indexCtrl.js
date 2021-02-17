f44de096d0b689ad7b077d09f7acdb6b41f450af
zpalawit.p <zpalawit.p@pttdigital.com>
2021-02-17 22:14:31 +0700

    add page A4 and input form

--- app/controllers/indexCtrl.js
+++ app/controllers/indexCtrl.js
@@ -2,12 +2,33 @@ var app = angular.module("MyApp", ["ngMaterial", "ngMessages"]);
 
 app.controller("ApplicationCtrl", function appCtrl($sce, $scope, $controller) {
   const sl = this;
-  $scope.currentNavItem = "page1";
+  sl.currentNavItem = "leaveForm";
 
-  $scope.goto = function (page) {
+  sl.goto = function (page) {
     $scope.status = "Goto " + page;
   };
-  
+
   sl.name = "MyApp";
   $scope.name2 = "TestTest";
+
+  $scope.user = {
+    title: 'Developer',
+    email: 'ipsum@lorem.com',
+    firstName: '',
+    lastName: '',
+    company: 'Google',
+    address: '1600 Amphitheatre Pkwy',
+    city: 'Mountain View',
+    state: null,
+    stateOfBirth: 'CA',
+    description: 'Loves TypeScript 💖',
+    postalCode: '94043',
+    licenseAccepted: true,
+    submissionDate: null,
+    marketingOptIn: true
+  };
+
+  $scope.gens = ('ชาย หญิง').split(' ').map(function (gen) {
+    return {abbrev: gen};
+  });
 });
