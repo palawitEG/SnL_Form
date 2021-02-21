b8f239ff74ce01d522a87240a8d5fe786f225c59
zpalawit.p <zpalawit.p@pttdigital.com>
2021-02-21 14:50:19 +0700

    add form

--- app/controllers/indexCtrl.js
+++ app/controllers/indexCtrl.js
@@ -2,53 +2,15 @@ var app = angular.module("MyApp", ["ngMaterial", "ngMessages"]);
 
 app.controller("ApplicationCtrl", function appCtrl($sce, $scope, $controller) {
   const sl = this;
+  let dateNow = new Date();
   sl.currentNavItem = "leaveForm";
 
   sl.goto = function (page) {
     $scope.status = "Goto " + page;
   };
-
-  sl.name = "MyApp";
-  $scope.name2 = "TestTest";
-
-  $scope.documentParam = {
-    NO: 1234,
-    docDate: getDateNowTH(),
-    subject: "abcd",
-    toEmployer1: "",
-    toEmployer2: "",
-    myName: "",
-    surName: "",
-    startWorkDate: getDateNowTH(),
-    endWorkDate: getDateNowTH(),
-    currentPosition: "God",
-    dept: "Heaven",
-    leaveReason: "cuz Money",
-  }
-
-  $scope.user = {
-    title: 'Developer',
-    email: 'ipsum@lorem.com',
-    firstName: '',
-    lastName: '',
-    company: 'Google',
-    address: '1600 Amphitheatre Pkwy',
-    city: 'Mountain View',
-    state: null,
-    stateOfBirth: 'CA',
-    description: 'Loves TypeScript 💖',
-    postalCode: '94043',
-    licenseAccepted: true,
-    submissionDate: null,
-    marketingOptIn: true
-  };
-
-  $scope.gens = ('ชาย หญิง').split(' ').map(function (gen) {
-    return {abbrev: gen};
-  });
-
-  function getDateNowTH() {
-    now = new Date();
+  
+  sl.getDateNowTH = function(now) {
+    // now = new Date();
     var thday = new Array(
       "อาทิตย์",
       "จันทร์",
@@ -81,4 +43,45 @@ app.controller("ApplicationCtrl", function appCtrl($sce, $scope, $controller) {
       (now.getFullYear() + 543)
     );
   };
+
+  sl.name = "MyApp";
+  $scope.name2 = "TestTest";
+
+  $scope.documentParam = {
+    NO: 1234,
+    docDate: sl.getDateNowTH(dateNow),
+    subject: "abcd",
+    toEmployer1: "",
+    toEmployer2: "",
+    titleName: "",
+    myName: "",
+    surName: "",
+    startWorkDate: "(ระบุวันที่เริ่มทำงาน)",
+    endWorkDate: "(ระบุวันที่เริ่มทำงาน)",
+    currentPosition: "God",
+    dept: "Heaven",
+    leaveReason: "cuz Money",
+  }
+
+  $scope.user = {
+    title: 'Developer',
+    email: 'ipsum@lorem.com',
+    firstName: '',
+    lastName: '',
+    company: 'Google',
+    address: '1600 Amphitheatre Pkwy',
+    city: 'Mountain View',
+    state: null,
+    stateOfBirth: 'CA',
+    description: 'Loves TypeScript 💖',
+    postalCode: '94043',
+    licenseAccepted: true,
+    submissionDate: null,
+    marketingOptIn: true
+  };
+
+  $scope.gens = ('ชาย หญิง').split(' ').map(function (gen) {
+    return {abbrev: gen};
+  });
+
 });
