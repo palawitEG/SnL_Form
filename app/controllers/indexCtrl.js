09050b656858a4aa9a2d0615996847a4eab78178
zpalawit.p <zpalawit.p@pttdigital.com>
2021-02-24 14:07:35 +0700

    edit form and add pdfmake

--- app/controllers/indexCtrl.js
+++ app/controllers/indexCtrl.js
@@ -1,87 +1,112 @@
 var app = angular.module("MyApp", ["ngMaterial", "ngMessages"]);
 
-app.controller("ApplicationCtrl", function appCtrl($sce, $scope, $controller) {
-  const sl = this;
-  let dateNow = new Date();
-  sl.currentNavItem = "leaveForm";
-
-  sl.goto = function (page) {
-    $scope.status = "Goto " + page;
+app.config(function ($mdDateLocaleProvider) {
+  $mdDateLocaleProvider.formatDate = function (date) {
+    return moment(date).format("DD-MM-YYYY");
   };
-  
-  sl.getDateNowTH = function(now) {
-    // now = new Date();
-    var thday = new Array(
-      "อาทิตย์",
-      "จันทร์",
-      "อังคาร",
-      "พุธ",
-      "พฤหัส",
-      "ศุกร์",
-      "เสาร์"
-    );
-    var thmonth = new Array(
-      "มกราคม",
-      "กุมภาพันธ์",
-      "มีนาคม",
-      "เมษายน",
-      "พฤษภาคม",
-      "มิถุนายน",
-      "กรกฎาคม",
-      "สิงหาคม",
-      "กันยายน",
-      "ตุลาคม",
-      "พฤศจิกายน",
-      "ธันวาคม"
-    );
-
-    return (
-      now.getDate() +
-      " " +
-      thmonth[now.getMonth()] +
-      " " +
-      (now.getFullYear() + 543)
-    );
-  };
-
-  sl.name = "MyApp";
-  $scope.name2 = "TestTest";
-
-  $scope.documentParam = {
-    NO: 987654321,
-    docDate: sl.getDateNowTH(dateNow),
-    subject: "ABC Company",
-    toEmployer1: "A",
-    toEmployer2: "B",
-    titleName: "(ระบุคำนำหน้าชื่อ)",
-    myName: "(ระบุชื่อ)",
-    surName: "(ระบุนามสกุล)",
-    startWorkDate: "(ระบุวันที่เริ่มทำงาน)",
-    endWorkDate: "(ระบุวันที่เริ่มทำงาน)",
-    currentPosition: "(ระบุตำแหน่ง)",
-    dept: "(ระบุแผนก หรือฝ่าย)",
-    leaveReason: "(ระบุเหตุผล)",
-  }
-
-  $scope.user = {
-    title: 'Developer',
-    email: 'ipsum@lorem.com',
-    firstName: '',
-    lastName: '',
-    company: 'Google',
-    address: '1600 Amphitheatre Pkwy',
-    city: 'Mountain View',
-    state: null,
-    stateOfBirth: 'CA',
-    description: 'Loves TypeScript 💖',
-    postalCode: '94043',
-    licenseAccepted: true,
-    submissionDate: null,
-    marketingOptIn: true
-  };
-
-  $scope.gens = ('ชาย หญิง').split(' ').map(function (gen) {
-    return {abbrev: gen};
-  });
-
 });
+
+app.controller(
+  "ApplicationCtrl",
+  function appCtrl($sce, $scope, $controller, $log) {
+    const sl = this;
+    let dateNow = new Date();
+    sl.currentNavItem = "leaveForm";
+
+    initialAll();
+
+    $scope.documentParam = {
+      NO: 987654321,
+      docDate: sl.getDateNowTH(dateNow),
+      subject: "",
+      toEmployer1: "",
+      toEmployer2: "",
+      titleName: "",
+      myName: "",
+      surName: "",
+      startWorkDate: sl.getDateNowTH(dateNow),
+      endWorkDate: sl.getDateNowTH(dateNow),
+      currentPosition: "",
+      dept: "",
+      leaveReason: "",
+    };
+
+    $scope.gens = "ชาย หญิง".split(" ").map(function (gen) {
+      return { abbrev: gen };
+    });
+
+    pdfMake.fonts = {
+      THSarabunNew: {
+        normal: 'THSarabunNew.ttf',
+        bold: 'THSarabunNew-Bold.ttf',
+        italics: 'THSarabunNew-Italic.ttf',
+        bolditalics: 'THSarabunNew-BoldItalic.ttf'
+      }
+    }
+
+    //#region functions
+    function initialAll () {
+      sl.goto = function (page) {
+        $scope.status = "Goto " + page;
+      };
+  
+      sl.getDateNowTH = function (now) {
+        // now = new Date();
+        var thday = new Array(
+          "อาทิตย์",
+          "จันทร์",
+          "อังคาร",
+          "พุธ",
+          "พฤหัส",
+          "ศุกร์",
+          "เสาร์"
+        );
+        var thmonth = new Array(
+          "มกราคม",
+          "กุมภาพันธ์",
+          "มีนาคม",
+          "เมษายน",
+          "พฤษภาคม",
+          "มิถุนายน",
+          "กรกฎาคม",
+          "สิงหาคม",
+          "กันยายน",
+          "ตุลาคม",
+          "พฤศจิกายน",
+          "ธันวาคม"
+        );
+  
+        return (
+          now.getDate() +
+          " " +
+          thmonth[now.getMonth()] +
+          " " +
+          (now.getFullYear() + 543)
+        );
+      };
+  
+      sl.onDateChanged = function (d, key) {
+        if (key == 1) {
+          $scope.documentParam.startWorkDate = sl.getDateNowTH(d);
+        } else {
+          $scope.documentParam.endWorkDate = sl.getDateNowTH(d);
+        }
+      };
+  
+      sl.exportPDF = function () {
+        let docDefinition = {
+          content:[
+            { text: 'Palawit Palasak', alignment: "center"}
+          ],
+          defaultStyle:{
+            font: "THSarabunNew"
+          }
+        }
+        pdfMake.createPdf(docDefinition).download("form.pdf");
+      } 
+    }
+
+    //#endregion
+
+  }
+);
